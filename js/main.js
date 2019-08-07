4688c147d89dc7600b3f524401e37270db31d548
Andy Rojas Córdoba <andyrojasucr@gmail.com>
2019-08-07 01:50:25 -0600

    Add changes

--- js/main.js
+++ js/main.js
@@ -38,8 +38,6 @@ btn.on('click', function(e) {
 		}
 	};
 
-
-
 	// iPad and iPod detection	
 	var isiPad = function(){
 		return (navigator.platform.indexOf("iPad") != -1);
@@ -154,9 +152,53 @@ btn.on('click', function(e) {
 		});
 	};
 
+	var cfg = {
+        scrollDuration : 800, // smoothscroll duration
+        mailChimpURL   : ''   // mailchimp url
+	}
+	
+	var ssMenuOnScrolldown = function() {
+        
+        var hdr = $('.s-header'),
+            hdrTop = $('.s-header').offset().top;
 
-	// Animations
-	// Home
+		$(window).on('scroll', function() {
+
+            if ($(window).scrollTop() > hdrTop) {
+                hdr.addClass('sticky');
+            }
+            else {
+                hdr.removeClass('sticky');
+            }
+
+        });
+    };
+
+
+	var ssSmoothScroll = function() {
+        
+        $('.smoothscroll').on('click', function (e) {
+            var target = this.hash,
+            $target    = $(target);
+            
+                e.preventDefault();
+                e.stopPropagation();
+
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, cfg.scrollDuration, 'swing').promise().done(function () {
+
+                // check if menu is open
+                if ($('body').hasClass('menu-is-open')) {
+                    $('.header-menu-toggle').trigger('click');
+                }
+
+                window.location.hash = target;
+            });
+        });
+
+	};
+	
 	var homeAnimate = function() {
 		if ( $('#fh5co-home').length > 0 ) {	
 
@@ -184,6 +226,56 @@ btn.on('click', function(e) {
 
 		}
 	};
+	
+
+	var ssMenuOnScrolldown = function() {
+        
+        var hdr = $('.s-header'),
+            hdrTop = $('.s-header').offset().top;
+
+		$(window).on('scroll', function() {
+
+            if ($(window).scrollTop() > hdrTop) {
+                hdr.addClass('sticky');
+            }
+            else {
+                hdr.removeClass('sticky');
+            }
+
+        });
+    };
+
+
+   /* Mobile Menu
+    * ---------------------------------------------------- */ 
+    var ssMobileMenu = function() {
+
+		var toggleButton = $('.header-menu-toggle'),
+            nav = $('.header-nav-wrap');
+
+        toggleButton.on('click', function(event){
+			event.preventDefault();
+
+            toggleButton.toggleClass('is-clicked');
+            nav.slideToggle();
+        });
+
+        if (toggleButton.is(':visible')) nav.addClass('mobile');
+
+        $(window).on('resize', function() {
+            if (toggleButton.is(':visible')) nav.addClass('mobile');
+            else nav.removeClass('mobile');
+        });
+
+        nav.find('a').on("click", function() {
+
+            if (nav.hasClass('mobile')) {
+                toggleButton.toggleClass('is-clicked');
+                nav.slideToggle(); 
+            }
+        });
+
+    };
 
 	var exploreAnimate = function() {
 
@@ -456,6 +548,34 @@ btn.on('click', function(e) {
 		}
 	};
 
+	var ssWaypoints = function() {
+
+        var sections = $(".target-section"),
+            navigation_links = $(".header-main-nav li a");
+
+        sections.waypoint( {
+
+            handler: function(direction) {
+
+                var active_section;
+
+                active_section = $('section#' + this.element.id);
+
+                if (direction === "up") active_section = active_section.prevAll(".target-section").first();
+
+                var active_link = $('.header-main-nav li a[href="#' + active_section.attr("id") + '"]');
+
+                navigation_links.parent().removeClass("current");
+                active_link.parent().addClass("current");
+
+            },
+
+            offset: '25%'
+
+        });
+        
+    };
+
 	// Document on load.
 	$(function(){
 		parallax();
@@ -463,6 +583,12 @@ btn.on('click', function(e) {
 		clickMenu();
 		windowScroll();
 		navigationSection();
+
+        ssMenuOnScrolldown();
+		ssMobileMenu();
+		ssSmoothScroll();
+		ssMenuOnScrolldown();
+		ssWaypoints();
 		
 		// Animations
 		homeAnimate();
